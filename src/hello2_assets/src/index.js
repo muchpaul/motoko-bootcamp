ad2250f42cf7d8ee7d3372731779c185cb1392d1
muchpaul <paul.githubstuff@gmail.com>
2022-03-03 19:30:56 -0500

    first commit

---
+++ src/hello2_assets/src/index.js
@@ -0,0 +1,19 @@
+import { hello2 } from "../../declarations/hello2";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await hello2.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
